b8509e7907dd8b60a83e022806975fc0c05b032f
yassineferjani <yassineferjani9@gmail.com>
2023-05-22 19:20:39 +0100

    Added the day 9.

---
+++ Stage9/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        List<String> list =Read.readFile("D:\\AdventOfCode2022\\Stage9\\src\\main\\resources\\input9.txt");
+        System.out.println(list);
+    }
+}
