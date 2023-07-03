72dcafc6e140b2102e78d601090f4904b142626c
yassineferjani <yassineferjani9@gmail.com>
2023-07-03 22:31:19 +0100

    Added the day 12.

--- stage12/src/main/java/org/example/Main.java
+++ stage12/src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.io.FileNotFoundException;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException {
+        List<String> list = Read.readFile("D:\\AdventOfCode2022\\stage12\\src\\main\\resources\\input12.txt");
+        System.out.println(list);
     }
 }
