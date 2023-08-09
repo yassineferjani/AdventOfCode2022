a228673f61a4d2deec4c184f542cdb258e49fe86
yassineferjani <yassineferjani9@gmail.com>
2023-08-09 20:32:53 +0100

    Day 13 : Splited sublist

--- Stage13/src/main/java/org/example/service/Service.java
+++ Stage13/src/main/java/org/example/service/Service.java
@@ -3,7 +3,6 @@ package org.example.service;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Stack;
 
 public class Service {
 
@@ -30,14 +29,12 @@ public class Service {
 
     public static List<List<String>> extractSubList(String s){
         List<List<Integer>> lists = extractIndexList(s);
-        List<List<String>>listCombinaison = new ArrayList<>();
+        List<List<String>>listCombination = new ArrayList<>();
         int j = 0;
         while (!lists.get(0).isEmpty() && !lists.get(1).isEmpty()){
             int i = lists.get(0).size();
         if(lists.get(0).get(i-1)<lists.get(1).get(j)) {
-            int a = lists.get(0).get(i-1);
-            int b = lists.get(1).get(j);
-            listCombinaison.add(List.of(s.substring(a, b+1)));
+            listCombination.add(List.of(s.substring(lists.get(0).get(i-1), lists.get(1).get(j)+1)));
             lists.get(0).remove(i-1);
             lists.get(1).remove(j);
             j=0;
@@ -45,7 +42,7 @@ public class Service {
             j++;
         }
         }
-        return listCombinaison;
+        return listCombination;
      }
 
 
